f1b373975588670a72e5ee64630977e7c023f407
taher fattahi <taherfattahi11@gmail.com>
2020-07-26 15:44:15 +0430

    init commit

---
+++ ContextMenuDialogFragment.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Com.Yalantis.Contextmenu.Lib;
+using Com.Yalantis.Contextmenu.Lib.Interfaces;
+using Android.Support.V4.App;
+
+
+namespace ContextMenuXamarinAndroid
+{
+    class ContextMenuDialogFragment : DialogFragment, IOnItemClickListener, IOnItemLongClickListener
+    {
+
+        public static String TAG = typeof(ContextMenuDialogFragment).Name;
+        private static String BUNDLE_MENU_PARAMS = "BUNDLE_MENU_PARAMS";
+
+        private LinearLayout mWrapperButtons;
+        private LinearLayout mWrapperText;
+        private MenuAdapter mDropDownMenuAdapter;
+        private IOnMenuItemClickListener mItemClickListener;
+        private IOnMenuItemLongClickListener mItemLongClickListener;
+        private MenuParams mMenuParams;
+
+        public static int STYLE_NO_FRAME = 2;
+
+        public static ContextMenuDialogFragment NewInstance(MenuParams menuParams)
+        {
+            ContextMenuDialogFragment fragment = new ContextMenuDialogFragment();
+            Bundle args = new Bundle();
+            args.PutParcelable(BUNDLE_MENU_PARAMS, menuParams);
+            fragment.Arguments = args;
+            return fragment;
+        }
+
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+
+            SetStyle(StyleNoFrame, Resource.Style.MenuFragmentStyle);
+            if (Arguments != null)
+            {
+                mMenuParams = (MenuParams)Arguments.GetParcelable(BUNDLE_MENU_PARAMS);
+            }
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View rootView = inflater.Inflate(Resource.Layout.fragment_menu, container, false);
+
+            rootView.SetFitsSystemWindows(mMenuParams.FitsSystemWindow);
+            ((ViewGroup)rootView).SetClipToPadding(mMenuParams.ClipToPadding);
+            initViews(rootView);
+            Dialog.Window.ClearFlags(WindowManagerFlags.DimBehind);
+            initDropDownMenuAdapter();
+
+            Handler h = new Handler();
+            Action myAction = () =>
+            {
+                // your code that you want to delay here
+                mDropDownMenuAdapter.MenuToggle();
+            };
+            h.PostDelayed(myAction, mMenuParams.AnimationDelay);
+
+            if (mMenuParams.ClosableOutside)
+            {
+                rootView.FindViewById<RelativeLayout>(Resource.Id.root).Click += (sender, args) =>
+                {
+                    if (IsAdded)
+                    {
+                        DismissAllowingStateLoss();
+                    }
+                };
+            }
+
+            return rootView;
+        }
+
+        private void initViews(View view)
+        {
+            mWrapperButtons = view.FindViewById<LinearLayout>(Resource.Id.wrapper_buttons);
+            mWrapperText = view.FindViewById<LinearLayout>(Resource.Id.wrapper_text);
+        }
+
+        private void initDropDownMenuAdapter()
+        {
+            mDropDownMenuAdapter = new MenuAdapter(Activity, mWrapperButtons, mWrapperText,
+                    mMenuParams.MenuObjects, mMenuParams.ActionBarSize);
+            mDropDownMenuAdapter.SetOnItemClickListener(this);
+            mDropDownMenuAdapter.SetOnItemLongClickListener(this);
+            mDropDownMenuAdapter.SetAnimationDuration(mMenuParams.AnimationDuration);
+        }
+
+        private void close()
+        {
+            Handler h = new Handler();
+            Action myAction = () =>
+            {
+                DismissAllowingStateLoss();
+            };
+            h.PostDelayed(myAction, mMenuParams.AnimationDelay);
+        }
+
+        public void setItemLongClickListener(IOnMenuItemLongClickListener itemLongClickListener)
+        {
+            this.mItemLongClickListener = itemLongClickListener;
+        }
+
+        public void setItemClickListener(IOnMenuItemClickListener itemClickListener)
+        {
+            this.mItemClickListener = itemClickListener;
+        }
+
+        void IOnItemClickListener.OnClick(View v)
+        {
+            if (mItemClickListener != null)
+            {
+                mItemClickListener.OnMenuItemClick(v, ((ViewGroup)v.Parent).IndexOfChild(v));
+            }
+            close();
+        }
+
+        void IOnItemLongClickListener.OnLongClick(View v)
+        {
+            if (mItemLongClickListener != null)
+            {
+                mItemLongClickListener.OnMenuItemLongClick(v, ((ViewGroup)v.Parent).IndexOfChild(v));
+            }
+            close();
+        }
+    }
+}
---
+++ MainActivity.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Graphics;
+using Com.Yalantis.Contextmenu.Lib;
+using Android.Graphics.Drawables;
+using Android.App;
+using Android.Support.V7.App;
+using Com.Yalantis.Contextmenu.Lib.Interfaces;
+
+namespace ContextMenuXamarinAndroid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, IOnMenuItemClickListener
+    {
+
+        private Button btn;
+
+        private ContextMenuDialogFragment mMenuDialogFragment;
+        private Android.Support.V4.App.FragmentManager fragmentManager;
+        
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            btn = FindViewById<Button>(Resource.Id.btnClick);
+
+            fragmentManager = SupportFragmentManager;
+
+            btn.Click += (sender, args) =>
+            {
+                if (fragmentManager.FindFragmentByTag(ContextMenuDialogFragment.TAG) == null)
+                {
+                    mMenuDialogFragment.Show(fragmentManager, ContextMenuDialogFragment.TAG);
+                }
+            };
+
+            initMenuFragment();
+
+        }
+
+
+        private void initMenuFragment()
+        {
+            MenuParams menuParams = new MenuParams();
+            //menuParams.ActionBarSize = (int)Resources.GetDimension(Resource.Dimension.tool_bar_height);
+            menuParams.ActionBarSize = 150;
+            menuParams.MenuObjects = getMenuObjects();
+            menuParams.ClosableOutside = true;
+            mMenuDialogFragment = ContextMenuDialogFragment.NewInstance(menuParams);
+            mMenuDialogFragment.setItemClickListener(this);
+            //mMenuDialogFragment.setItemLongClickListener(this);
+        }
+
+        private List<MenuObject> getMenuObjects()
+        {
+            // You can use any [resource, bitmap, drawable, color] as image:
+            // item.setResource(...)
+            // item.setBitmap(...)
+            // item.setDrawable(...)
+            // item.setColor(...)
+            // You can set image ScaleType:
+            // item.setScaleType(ScaleType.FIT_XY)
+            // You can use any [resource, drawable, color] as background:
+            // item.setBgResource(...)
+            // item.setBgDrawable(...)
+            // item.setBgColor(...)
+            // You can use any [color] as text color:
+            // item.setTextColor(...)
+            // You can set any [color] as divider color:
+            // item.setDividerColor(...)
+
+            List<MenuObject> menuObjects = new List<MenuObject>();
+
+            MenuObject close = new MenuObject();
+            close.Resource = Resource.Drawable.icn_close;
+
+            MenuObject send = new MenuObject("Send message");
+            send.Resource = Resource.Drawable.icn_1;
+
+            MenuObject like = new MenuObject("Like profile");
+            Bitmap b = BitmapFactory.DecodeResource(Resources, Resource.Drawable.icn_2);
+            like.Bitmap = b;
+
+            MenuObject addFr = new MenuObject("Add to friends");
+            BitmapDrawable bd = new BitmapDrawable(Resources, BitmapFactory.DecodeResource(Resources, Resource.Drawable.icn_3));
+            addFr.Drawable = bd;
+
+            MenuObject addFav = new MenuObject("Add to favorites");
+            addFav.Resource = Resource.Drawable.icn_4;
+
+            MenuObject block = new MenuObject("Block user");
+            block.Resource = Resource.Drawable.icn_5;
+
+            menuObjects.Add(close);
+            menuObjects.Add(send);
+            menuObjects.Add(like);
+            menuObjects.Add(addFr);
+            menuObjects.Add(addFav);
+            menuObjects.Add(block);
+            return menuObjects;
+        }
+
+        public void OnMenuItemClick(View p0, int p1)
+        {
+            //throw new NotImplementedException();
+
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
